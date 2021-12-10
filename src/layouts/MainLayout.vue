101c46452b5a997231f59971dbeb0bef960ece4a
Luiz Eduardo <luizzeduardp@gmail.com>
2021-12-10 17:43:52 -0300

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-layout view="lHh lpR lFr">
+
+    <q-header reveal elevated class=" text-white" height-hint="98" style="background-color:rgb(88 32 149)">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="../assets/cerveja.png">
+          </q-avatar>
+          Eu Nunca!
+        </q-toolbar-title>
+      </q-toolbar>
+
+     
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" elevated>
+      <!-- drawer content -->
+      <q-card dark bordered class="bg-grey-9 my-card">
+      <q-card-section>
+        <div class="text-h6">Eu nunca Versão 2.0</div>
+        <div class="text-subtitle2">CWB GAME STUDIO</div>
+      </q-card-section>
+
+      <q-separator dark inset />
+
+      <q-card-section>
+       redes sociais
+      </q-card-section>
+    </q-card>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
